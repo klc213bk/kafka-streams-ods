3af7333bfbc97f6c93870e80ee2aa7ff34219a97
klc213bk <klc213bk@gmail.com>
2021-04-07 18:15:04 +0800

    initial

---
+++ src/main/java/com/transglobe/kafka/streams/ods/simple/Application.java
@@ -0,0 +1,59 @@
+package com.transglobe.kafka.streams.ods.simple;
+
+import java.util.Arrays;
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.KStream;
+
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println(">>> SimpleApplication is running !!!");
+		
+		Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-ods-simple");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        final StreamsBuilder builder = new StreamsBuilder();
+
+        KStream<String, String> source = builder.stream("etl.pmuser.t_production_detail");
+
+        source.map((key, value) -> {
+        	return new KeyValue<>(key, value);
+        })
+        .to("etl.pmuser.k_production_detail");
+
+        final Topology topology = builder.build();
+
+        final KafkaStreams streams = new KafkaStreams(topology, props);
+        final CountDownLatch latch = new CountDownLatch(1);
+
+        // attach shutdown handler to catch control-c
+        Runtime.getRuntime().addShutdownHook(new Thread("streams-shutdown-hook") {
+            @Override
+            public void run() {
+                streams.close();
+                latch.countDown();
+            }
+        });
+
+        try {
+            streams.start();
+            latch.await();
+        } catch (Throwable e) {
+            System.exit(1);
+        }
+        System.exit(0);
+        
+	}
+
+}
---
+++ src/main/java/com/transglobe/kafka/streams/ods/simple/SimpleApplication.java
@@ -0,0 +1,9 @@
+package com.transglobe.kafka.streams.ods.simple;
+
+public class SimpleApplication {
+
+	public static void main(String[] args) {
+		System.out.println(">>> SimpleApplication is running !!!");
+	}
+
+}
