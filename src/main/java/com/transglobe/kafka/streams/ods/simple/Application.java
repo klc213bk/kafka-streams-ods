9a8c78c3a040cd7427edac8706fcfba591818b1b
feib <feib@localhost>
2021-04-14 02:39:32 +0800

    initial

--- src/main/java/com/transglobe/kafka/streams/ods/simple/Application.java
+++ src/main/java/com/transglobe/kafka/streams/ods/simple/Application.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
 
@@ -23,6 +24,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.transglobe.kafka.streams.ods.common.ProductionDetail;
 
+import net.sf.jsqlparser.JSQLParserException;
+import net.sf.jsqlparser.expression.Expression;
+import net.sf.jsqlparser.parser.CCJSqlParserUtil;
+import net.sf.jsqlparser.schema.Column;
+import net.sf.jsqlparser.statement.Statement;
+import net.sf.jsqlparser.statement.update.Update;
+
 
 public class Application {
 	static final Logger logger = LoggerFactory.getLogger(Application.class);
@@ -41,7 +49,7 @@ public class Application {
 			String sinkTable = prop.getProperty("sink.table");
 			String sinkSegOwner = sinkTable.split("\\.")[0];
 			String sinkTableName = sinkTable.split("\\.")[1];
-
+			
 			Properties props = new Properties();
 			props.put(StreamsConfig.APPLICATION_ID_CONFIG, applicationId);
 			props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
@@ -53,49 +61,60 @@ public class Application {
 			KStream<String, String> source = builder.stream(sourceTopic);
 
 			CountRecord countRecord = new CountRecord();
-			
+
 			source.map((key, value) -> {
-//				logger.warn("key={}", key);
-//				logger.warn("value={}", value);
+				//				logger.warn("key={}", key);
+				logger.warn("value={}", value);
 				countRecord.cnt = countRecord.cnt + 1;
-				logger.warn(">>>record procedded={}", countRecord.cnt);
-				
+				logger.warn(">>>record processed={}", countRecord.cnt);
+
 				ObjectMapper objectMapper = new ObjectMapper();
 				try {
 					JsonNode jsonNode = objectMapper.readTree(value);
-					String redoStr = jsonNode.get("payload").get("SQL_REDO").asText();
-					String sourceSegOwner = jsonNode.get("payload").get("SEG_OWNER").asText();
-					String sourceTableName = jsonNode.get("payload").get("TABLE_NAME").asText();
-					String sourceTableStr = String.format("\"%s\".\"%s\"", sourceSegOwner, sourceTableName);
-					String sinkTableStr = String.format("\"%s\".\"%s\"", sinkTable.split("\\.")[0], sinkTable.split("\\.")[1]);
-					ObjectNode on = (ObjectNode)jsonNode.get("payload");
-					on.put("SINK_SEG_OWNER", sinkSegOwner);
-					on.put("SINK_TABLE_NAME", sinkTableName);
-					on.put("SQL_REDO", redoStr.replace(sourceTableStr, sinkTableStr));
-					
-					String dataContent = jsonNode.get("payload").get("data").toString();
-//					logger.warn("dataContent={}", dataContent);
-					ObjectMapper objectMapper2 = new ObjectMapper();
-					ProductionDetail detail = objectMapper2.readValue(dataContent, ProductionDetail.class);
-					
-					java.util.Date utilDate = new java.util.Date();
-					LocalDate localDate = LocalDate.now();
-					Date date = Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
-					detail.setDataDate(date);
-					detail.setTblUpdTime(utilDate);
-					
-					on.put("SINK_DATA", objectMapper2.writeValueAsString(detail));
-					
-//					logger.warn("detail={}", ToStringBuilder.reflectionToString(detail));
-	
-					return new KeyValue<>(key, jsonNode.toString());
+					String operation = jsonNode.get("payload").get("OPERATION").asText();
+					if ("UPDATE".equals(operation)) {
+						String updateSql = getUpdateSql(jsonNode, sinkTable);
+						ObjectNode on = (ObjectNode)jsonNode.get("payload");
+						on.put("SINK_SQL_REDO", updateSql);
+						return new KeyValue<>(key, jsonNode.toString());
+					} else if ("INSERT".equals(operation)) {
+
+						String redoStr = jsonNode.get("payload").get("SQL_REDO").asText();
+						String sourceSegOwner = jsonNode.get("payload").get("SEG_OWNER").asText();
+						String sourceTableName = jsonNode.get("payload").get("TABLE_NAME").asText();
+						String sourceTableStr = String.format("\"%s\".\"%s\"", sourceSegOwner, sourceTableName);
+						String sinkTableStr = String.format("\"%s\".\"%s\"", sinkTable.split("\\.")[0], sinkTable.split("\\.")[1]);
+						ObjectNode on = (ObjectNode)jsonNode.get("payload");
+						on.put("SINK_SEG_OWNER", sinkSegOwner);
+						on.put("SINK_TABLE_NAME", sinkTableName);
+						on.put("SQL_REDO", redoStr.replace(sourceTableStr, sinkTableStr));
+
+						String dataContent = jsonNode.get("payload").get("data").toString();
+						//					logger.warn("dataContent={}", dataContent);
+						ObjectMapper objectMapper2 = new ObjectMapper();
+						ProductionDetail detail = objectMapper2.readValue(dataContent, ProductionDetail.class);
+
+						java.util.Date utilDate = new java.util.Date();
+						LocalDate localDate = LocalDate.now();
+						Date date = Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+						detail.setDataDate(date);
+						detail.setTblUpdTime(utilDate);
+
+						on.put("SINK_DATA", objectMapper2.writeValueAsString(detail));
+
+						//					logger.warn("detail={}", ToStringBuilder.reflectionToString(detail));
+
+						return new KeyValue<>(key, jsonNode.toString());
+					} else {
+						return new KeyValue<>(key, jsonNode.toString());
+					}
 				} catch (Exception e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 					logger.error(e.getMessage(), e);
 					return new KeyValue<>("", "");
 				} 
-				
+
 			})
 			.to(sinkTopic);
 
@@ -122,5 +141,34 @@ public class Application {
 		System.exit(0);
 
 	}
-
+	
+	private static String getUpdateSql(JsonNode jsonNode, String sinkFullTableName) throws JSQLParserException {
+		String redoStr = jsonNode.get("payload").get("SQL_REDO").asText();
+		Statement statement = CCJSqlParserUtil.parse(redoStr);
+		Update update = (Update) statement;
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append("UPDATE " + sinkFullTableName + " SET ");
+		List<Column> columns = update.getColumns();
+		List<Expression> expressions = update.getExpressions();
+		int size = expressions.size();
+		String[] values = new String[size];
+		for (int i = 0; i < size; i++) {
+			values[i] = expressions.get(i).toString();
+			System.out.println("value i:" + values[i]);
+		}
+		for (int i = 0; i < columns.size(); i++) {
+			// entry.put(trimQuotes(columns.get(i).toString()), trimQuotes(values[i]));
+			System.out.println("columns i:" + columns.get(i));
+			sb.append(columns.get(i) + "=" + values[i].toString() + ",");
+		}
+		sb.append("\"DATA_DATE\"=TIMESTAMP ' " + "2021-01-01 00:00:00" + "', ");
+		sb.append("\"TBL_UPD_TIME\"=TIMESTAMP ' " + "2021-01-01 13:34:31" + "' ");
+		
+		Expression where = update.getWhere();
+		System.out.println("where :" + where.toString());
+		sb.append("WHERE " + where.toString());
+		
+		return sb.toString();
+	}
 }
--- src/main/java/com/transglobe/kafka/streams/ods/simple/TestJson.java
+++ src/main/java/com/transglobe/kafka/streams/ods/simple/TestJson.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.Properties;
 
 import org.slf4j.Logger;
@@ -13,13 +14,23 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
+import net.sf.jsqlparser.JSQLParserException;
+import net.sf.jsqlparser.expression.BinaryExpression;
+import net.sf.jsqlparser.expression.Expression;
+import net.sf.jsqlparser.parser.CCJSqlParserUtil;
+import net.sf.jsqlparser.schema.Column;
+import net.sf.jsqlparser.schema.Table;
+import net.sf.jsqlparser.statement.Statement;
+import net.sf.jsqlparser.statement.update.Update;
+
 public class TestJson {
 	static final Logger logger = LoggerFactory.getLogger(TestJson.class);
-	
+
 	public static void main(String[] args)  {
-		
+
 		try {
-			test();
+			//	test();
+			parseUpdate();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -27,43 +38,80 @@ public class TestJson {
 	}
 	public static void test() throws Exception {
 		Properties prop = ConfigUtils.getProperties("config.properties");
-	    String sinkTable = prop.getProperty("sink.table");
-	    String sinkSegOwner = sinkTable.split("\\.")[0];
-	    String sinkTableName = sinkTable.split("\\.")[1];
-	    logger.warn(" sinkTable={}", sinkTable);
-	    logger.warn("sinkSegOwner={}, sinkTableName={}", sinkSegOwner, sinkTableName);
-		
+		String sinkTable = prop.getProperty("sink.table");
+		String sinkSegOwner = sinkTable.split("\\.")[0];
+		String sinkTableName = sinkTable.split("\\.")[1];
+		logger.warn(" sinkTable={}", sinkTable);
+		logger.warn("sinkSegOwner={}, sinkTableName={}", sinkSegOwner, sinkTableName);
+
 		Path path = Paths.get("/home/oracle/gitrepository/kafka-streams-ods/src/main/resources/testjson.txt");
 
-	    String json = Files.readAllLines(path).get(0);
-	    logger.warn("json={}", json);
-	    
-	    
-	    ObjectMapper objectMapper = new ObjectMapper();
-	    JsonNode jsonNode = objectMapper.readTree(json);
-	    System.out.println("jsonNode:" + jsonNode);
-	    
-	    System.out.println("schema:" + jsonNode.get("schema"));
-	    System.out.println("payload:" + jsonNode.get("payload"));
+		String json = Files.readAllLines(path).get(0);
+		logger.warn("json={}", json);
 
-	    System.out.println("SEG_OWNER:" + jsonNode.get("payload").get("SEG_OWNER").asText());
-	    System.out.println("TABLE_NAME:" + jsonNode.get("payload").get("TABLE_NAME").asText());
-	    System.out.println("SQL_REDO:" + jsonNode.get("payload").get("SQL_REDO").asText());
-	    String redoStr = jsonNode.get("payload").get("SQL_REDO").asText();
-	    String sourceSegOwner = jsonNode.get("payload").get("SEG_OWNER").asText();
-	    String sourceTableName = jsonNode.get("payload").get("TABLE_NAME").asText();
-	    String sourceSchemaStr = String.format("\"%s\".\"%s\"", sourceSegOwner, sourceTableName);
-	    String afterSchemaStr = String.format("\"%s\".\"%s\"", sinkTable.split("\\.")[0], sinkTable.split("\\.")[1]);
-	    logger.warn("after repacestr:" + redoStr.replace(sourceSchemaStr, afterSchemaStr));
-	    ObjectNode on = (ObjectNode)jsonNode.get("payload");
-	    on.put("SEG_OWNER", sinkSegOwner);
-	    on.put("TABLE_NAME", sinkTableName);
-	    on.put("SQL_REDO", redoStr.replace(sourceSchemaStr, afterSchemaStr));
-	    logger.warn("after schema str={}", jsonNode);
-	    
-//	    
-//	    redoStr.re
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		JsonNode jsonNode = objectMapper.readTree(json);
+		System.out.println("jsonNode:" + jsonNode);
+
+		System.out.println("schema:" + jsonNode.get("schema"));
+		System.out.println("payload:" + jsonNode.get("payload"));
+
+		System.out.println("SEG_OWNER:" + jsonNode.get("payload").get("SEG_OWNER").asText());
+		System.out.println("TABLE_NAME:" + jsonNode.get("payload").get("TABLE_NAME").asText());
+		System.out.println("SQL_REDO:" + jsonNode.get("payload").get("SQL_REDO").asText());
+		String redoStr = jsonNode.get("payload").get("SQL_REDO").asText();
+		String sourceSegOwner = jsonNode.get("payload").get("SEG_OWNER").asText();
+		String sourceTableName = jsonNode.get("payload").get("TABLE_NAME").asText();
+		String sourceSchemaStr = String.format("\"%s\".\"%s\"", sourceSegOwner, sourceTableName);
+		String afterSchemaStr = String.format("\"%s\".\"%s\"", sinkTable.split("\\.")[0], sinkTable.split("\\.")[1]);
+		logger.warn("after repacestr:" + redoStr.replace(sourceSchemaStr, afterSchemaStr));
+		ObjectNode on = (ObjectNode)jsonNode.get("payload");
+		on.put("SEG_OWNER", sinkSegOwner);
+		on.put("TABLE_NAME", sinkTableName);
+		on.put("SQL_REDO", redoStr.replace(sourceSchemaStr, afterSchemaStr));
+		logger.warn("after schema str={}", jsonNode);
+
+		//	    
+		//	    redoStr.re
 
 	}
+	
+	public static void parseUpdate() throws JSQLParserException {
+		String sql = "Update T_PRODUCTON_DETAIL SET ORDER_ID = 2, PRODUCT_ID='sss' where detail_id = 23456";
+		Statement statement = CCJSqlParserUtil.parse(sql);
+		Update update = (Update) statement;
 
+		// parse table name
+		List<net.sf.jsqlparser.schema.Table> tables = update.getTables();
+		String tableName = tables.get(0).getName();
+		System.out.println("tableName:" + tableName);
+		// table.setTableName(tableName);
+
+		// parse cloumns
+		List<Column> columns = update.getColumns();
+		List<Expression> expressions = update.getExpressions();
+		int size = expressions.size();
+		String[] values = new String[size];
+		for (int i = 0; i < size; i++) {
+			values[i] = expressions.get(i).toString();
+			System.out.println("value i:" + values[i]);
+		}
+		for (int i = 0; i < columns.size(); i++) {
+			// entry.put(trimQuotes(columns.get(i).toString()), trimQuotes(values[i]));
+			System.out.println("columns i:" + columns.get(i));
+		}
+		Expression where = update.getWhere();
+		System.out.println("where :" + where.toString());
+		
+		//	    
+		//	    // parse where clause
+		//	    Expression where = update.getWhere();
+		//	    if (where != null) {
+		//	        BinaryExpression expr2 = (BinaryExpression) (where);
+		//	        handleExpression(condition, expr2);
+		//	    }
+		//	    Limit limit = update.getLimit();
+		//	    parseLimit(condition, limit);
+	}
 }
